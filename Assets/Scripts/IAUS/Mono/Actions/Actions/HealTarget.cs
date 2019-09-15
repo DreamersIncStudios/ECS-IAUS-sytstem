5324387d283114ea1733841ae6e68cada2936386
The Dreamer <dreamers.inc12@gmail.com>
2019-09-15 12:19:02 -0400

    Adding Basic Editor Window to examine Consideration and Action Utility Scores in realtime without Debug.log to console

---
+++ Assets/Scripts/IAUS/Mono/Actions/Actions/HealTarget.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IAUS.Considerations;
+namespace IAUS.Actions
+{
+    public class HealTarget : ActionBase
+    {
+        public Target Target;
+
+        public override void Setup()
+        {
+            base.Setup();
+            Considerations.Add(new CharacterHealth
+            {
+                Agent = Agent,
+                NameId = "Health",
+                responseType = ResponseType.Logistic,
+                M = 5,
+                K = -.85f,
+                B = 1f,
+                C = .4f
+            });
+            Considerations.Add(new CharacterMana
+            {
+                Agent = Agent,
+                NameId = "Mana",
+                responseType = ResponseType.Logistic,
+                M = 5,
+                K = .85f,
+                B = 0.155f,
+                C = .4f
+            });
+        }
+        public override void OnExit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnStart()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+ 
+
+
+
+    }
+
+
+    public enum Target {
+        Player,
+        Self,
+        Allies
+    }
+}
---
+++ Assets/Scripts/IAUS/Mono/Context/Editor/UTEditorWindow.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace IAUS.Editors
+{
+    public class UTEditorWindow : EditorWindow
+    {
+        [MenuItem("IAUS/Utility Window")]
+        public static void ShowWindow() {
+            EditorWindow.GetWindow(typeof(UTEditorWindow));
+        }
+
+        Vector2 scrollPosition;
+
+        private void OnGUI()
+        {
+            scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+
+            foreach (GameObject GO in Selection.gameObjects)
+            {
+                UtilitySystem US = GO.GetComponent<UtilitySystem>();
+                if (US != null)
+                {
+
+                 
+                    GUILayout.BeginVertical("box");
+
+                    GUILayout.Label(GO.name);
+
+                    foreach (ActionBase Action in US.Actions)
+                    {
+                        GUILayout.BeginVertical("box");
+                        GUILayout.Label(Action.NameId + ": " + Action.TotalScore);
+                        foreach (ConsiderationBase consideration in Action.Considerations)
+                        {
+                            GUILayout.Label("\t" + consideration.NameId + ": " + consideration.Score);
+                        }
+                        GUILayout.EndVertical();
+
+                    }
+
+                    GUILayout.EndVertical();
+                }
+            }
+                GUILayout.EndScrollView();
+            
+        }
+
+    }
+}
